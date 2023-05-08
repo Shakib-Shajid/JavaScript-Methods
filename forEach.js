5b73dc46ebc0e5b0ebf841bbd2175482f7351462
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-05-08 23:28:01 +0600

    forEach()

--- forEach.js
+++ forEach.js
@@ -1,7 +1,15 @@
+// First Code
 const arr = ['learn', 'with', 'sumit'];
 arr.forEach(element => console.log(element) );
 
+// Second Code
 const array = ['abc', 'def', 'ghi'];
-array.forEach(function(currvalue){
-    console.log(currvalue);
+array.forEach(function(currValue){
+    console.log(currValue);
 })
+
+// Third Code
+const newArr = [10, 20, 30, 40, 50];
+newArr.forEach(function(val, i, arr){
+    console.log("value = " + val +" index = "+ i + " " +" Array = " +  arr);
+});
