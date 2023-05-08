21a955ad613da17853959aeba4ff476ee04fd6d2
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-05-08 23:21:24 +0600

    forEach() basic

---
+++ forEach.js
@@ -0,0 +1,7 @@
+const arr = ['learn', 'with', 'sumit'];
+arr.forEach(element => console.log(element) );
+
+const array = ['abc', 'def', 'ghi'];
+array.forEach(function(currvalue){
+    console.log(currvalue);
+})
